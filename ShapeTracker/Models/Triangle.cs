aaeaf94cae885849a07b89d474da11bd9ee2e14c
Ashe Urban <ashe@goldentongue.com>
2023-02-22 10:02:32 -0800

    Update personal ShapeTracker project.

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -1,25 +1,19 @@
 using System.Collections.Generic;
 
-namespace ShapeTracker.Models
+namespace ShapeTracker.Models 
 {
-  public class Triangle
+  public class Triangle 
   {
-
-    //Side1 is an example of using a C# property to provide access to a private field.
     private int _side1;
-
     public int Side1
     {
       get { return _side1; }
       set { _side1 = value; }
     }
-    
-    //Side2 is an example of an auto-implemented property.
+
     public int Side2 { get; set; }
-
-    private int _side3;
+    private int _side3; 
     private static List<Triangle> _instances = new List<Triangle> {};
-
     public Triangle(int length1, int length2, int length3)
     {
       _side1 = length1;
@@ -28,47 +22,43 @@ namespace ShapeTracker.Models
       _instances.Add(this);
     }
 
-
-//_side3 is an example using getter and setter methods to access a private field, using no properties.
     public int GetSide3()
     {
       return _side3;
     }
 
-    public void SetSide3(int newSide)
+    public void SetSide3(int newValue)
     {
-      _side3 = newSide;
+      _side3 = newValue;
     }
 
     public string CheckType()
     {
-      if ((_side1 > (Side2+_side3)) || (Side2 > (_side1+_side3)) || (_side3 > (_side1+Side2)))
+      if ((Side1 > (Side2 + _side3)) || (Side2 > (Side1 + _side3)) || (_side3 > (Side1 + Side2))) 
       {
-      return "not a triangle";
+        return "not a triangle";
+      } 
+      else if ((Side1 != Side2) && ((Side1 != _side3)) && ((Side2 != _side3))) 
+      {
+        return "scalene triangle";
       }
-      else if ((_side1 !=Side2) && ((_side1 !=_side3)) && ((Side2 !=_side3)))
+      else if ((Side1 == Side2) && (Side1 == _side3)) 
       {
-      return "scalene triangle";
-      }
-      else if ((_side1 ==Side2) && (Side2 ==_side3))
+        return "equilateral triangle";
+      } 
+      else
       {
-      return "equilateral triangle";
-      }
-      else 
-      {
-      return "isosceles triangle";
+        return "isosceles triangle";
       }
     }
 
-      public static List<Triangle> GetAll()
+    public static List<Triangle> GetAll()
     {
       return _instances;
     }
-
     public static void ClearAll()
     {
       _instances.Clear();
     }
-
   }
 }
